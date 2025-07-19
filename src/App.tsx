270df733d4757c5773013c291203858dbd321a0a
violet-markey <srikrishna.cs22@bmsce.ac.in>
2025-07-20 02:51:53 +0530

    Restore full app functionality - all components and routing working!

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,46 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Home from "./pages/Home";
+import Recipes from "./pages/Recipes";
+import Saved from "./pages/Saved";
+import NotFound from "./pages/NotFound";
+import Dieticians from "./pages/Dieticians";
+import Blogs from "./pages/Blogs";
+import Auth from "./pages/Auth";
+import About from "./pages/About";
+import RecipeDetail from "./pages/RecipeDetail";
+import ForgotPassword from "./pages/ForgotPassword";
+import AuthPage from "./pages/AuthPage";
+
+const queryClient = new QueryClient();
 
 const App = () => (
-  <BrowserRouter>
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Layout>
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/recipes" element={<Recipes />} />
+            <Route path="/saved" element={<Saved />} />
+            <Route path="/dieticians" element={<Dieticians />} />
+            <Route path="/blogs" element={<Blogs />} />
+            <Route path="/auth" element={<AuthPage />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/recipe/:id" element={<RecipeDetail />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
 );
 
-// Test comment: Auto-deploy should trigger when this is pushed
 export default App;
